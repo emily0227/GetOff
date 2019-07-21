0320449986debcea9c2d7d2689d1cdff64386dd4
emily0227 <hmlove0227@naver.com>
2019-07-21 15:12:41 +0900

    2차 작업

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,12 +2,28 @@ package com.example.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.start_page);
+       /* MyClick myclick = new MyClick(); */
+        findViewById(R.id.home).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+    /* class MyClick implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }*/
+
 }
