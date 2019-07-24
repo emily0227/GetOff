5d12e865c72e53907ae53bee9f56caa75cac64b8
emily0227 <hmlove0227@naver.com>
2019-07-24 13:53:30 +0900

    툴바까지 완성

--- app/src/main/java/com/example/myapplication/SubwayMain.java
+++ app/src/main/java/com/example/myapplication/SubwayMain.java
@@ -1,34 +1,43 @@
 package com.example.myapplication;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class SubwayMain extends AppCompatActivity {
+
 
     Toolbar myToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.start_page);
+        setContentView(R.layout.subway_main);
 
         myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
 
         getSupportActionBar().setTitle("Get Off");
+        /* MyClick myclick = new MyClick(); */
 
-       /* MyClick myclick = new MyClick(); */
-        findViewById(R.id.textStart).setOnClickListener(new View.OnClickListener() {
+
+        findViewById(R.id.bus).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, SubwayMain.class);
-                startActivity(intent);
-
+                setContentView(R.layout.bus_main);
             }
         });
+
+    /*    findViewById(R.id.subway).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setContentView(R.layout.subway_main);
+            }
+        });*/
+    }
+
+    private class DefaultInput {
     }
     /* class MyClick implements View.OnClickListener {
 
